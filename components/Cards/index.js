deb7037b8d7b999203aaf0f4dcb33774192139f5
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-02-14 14:55:04 -0600

    done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,56 +1,36 @@
-
-// STEP 3: Create Article cards.
-// -----------------------
-// Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
-// You will be creating a component for each 'article' in the list.
-// This won't be as easy as just iterating over an array though.
-// Create a function that will programmatically create the following DOM component:
-//
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {authors name}</span>
-//   </div>
-// </div>
-//
-// Create a card for each of the articles and add the card to the DOM.
-
+const cards = document.querySelector( '.cards-container' );
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
-.then(response => {
-    console.log(response.data.articles.javascript.headline)
-})
+    .then( response => {
+         res = Object.values( response.data.articles );
+         res.forEach( el => {
+            el.forEach( e => {
+                cards.appendChild( CreateCard( e ) );
+            });
+         });
+    })
+    .catch( error => { consols.log(`Error: ${error}`) });
 
-function Articles(card, headline, author, img){
-//create elements
-const cardsContainer = document.createElement
-const _card = document.createElement('div');
-const _headline = document.createElement('div');
-const _author = document.createElement('div');
-const _imgContainer = document.createElement('div');
-const _imgSrc = document.createElement('img src');
-const _span = document.createElement('span');
+const CreateCard = ( obj ) => {
+    const card = document.createElement( 'div'  );
+    const head = document.createElement( 'div'  );
+    const auth = document.createElement( 'div'  );
+    const cont = document.createElement( 'div'  );
+    const img  = document.createElement( 'img'  );
+    const by   = document.createElement( 'span' );
 
-//classNames
-_card.classList.add('card');
-_headline.classList.add('headline');
-_author.classList.add('author');
-_imgContainer.classList.add('img-container');
+    card.classList.add( 'card'          );
+    head.classList.add( 'headline'      );
+    auth.classList.add( 'author'        );
+    cont.classList.add( 'img-container' );
 
-//textContent
-_headline.textContent = headline;
-_imgSrc.src = img;
-_span.textContent = `By ${author}`;
-
-//append
-_card.appendChild(_headline);
-_card.appendChild(_author);
-_author.appendChild(_imgContainer);
-_imgContainer.appendChild(_imgSrc);
-_imgContainer.appendChild(_span);
-
-}
-
+    head.textContent = obj.headline;
+    by.textContent   = `By ${ obj.authorName }`;
+    img.setAttribute( 'src', obj.authorPhoto );
+    
+    card.appendChild( head );
+    card.appendChild( auth );
+    auth.appendChild( cont );
+    cont.appendChild( img  );
+    auth.appendChild( by   );
+    return card;
+};
